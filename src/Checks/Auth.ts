8cdecef9ab45214b02029727ac97418020052d5f
Sudhanshu-choudhary <138672423+Sudhanshu-choudhary@users.noreply.github.com>
2024-11-05 20:14:16 +0530

    Update Auth.ts

--- src/Checks/Auth.ts
+++ src/Checks/Auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AUTH_BASE_URL, AGENT } from "../../constants";
 import axiosInstance from "@/lib/axios";
 import toast from "react-hot-toast";
